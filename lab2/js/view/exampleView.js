bfd9e5dc73f2df0e65d51f7b72acb59a53e9b814
Rasmus Elmgren <elmgren93@gmail.com>
2016-02-04 11:20:46 +0100

    changed dishes to appear 5 per row

--- lab2/js/view/exampleView.js
+++ lab2/js/view/exampleView.js
@@ -3,9 +3,35 @@ var ExampleView = function (container, model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#mid-lower");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
+	// this.numberOfGuests = container.find("#mid-lower");
+	// this.plusButton = container.find("#plusGuest");
+	// this.minusButton = container.find("#minusGuest");
+
+	$(document).ready(function() {
+		$("#numberOfGuests").val( model.getNumberOfGuests() );
+
+		$selectValue = $("#mid-upper select").val();
+		$searchValue = $("dish-search").val();
+
+		$.each( model.getAllDishes( $selectValue, $searchValue ).prevObject , function(index, object) {
+
+			$("#mid-lower .container").append(
+				"<div class=\"dish col-sm-15\" id=\"" + object.id + "\">" +
+					"<div class=\"dish-header\">" +  
+						"<img src=\"images/" + object.image + "\">" +
+						"<p>" + object.name + "</p>" +
+					"</div>" +
+					"<div class=\"dish-description\"><p>" + object.description + "</p> </div>" +
+				"</div>");
+
+			console.log(object.image + " " + object.name + " " + object.description);
+		});
+	})
+
+
+	$("#numberOfGuests").keyup(function() {
+		model.setNumberOfGuests( $this.val() );
+	});
 
 }
  
