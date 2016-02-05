7c1a515e7b72228c966032471eadcafd0456a5d0
Rasmus Elmgren <elmgren93@gmail.com>
2016-02-05 15:19:28 +0100

    tinkered with dinnerOverview

--- lab2/js/view/overviewView.js
+++ lab2/js/view/overviewView.js
@@ -5,29 +5,48 @@ var OverviewView = function (container, model, selectedMenu) {
 		$("#overview-headline span").html( model.getNumberOfGuests() + " people");
 
 		$totalPrice = 0;
+		$lengthOfArray = selectedMenu.length;
 		//model.getFullMenu();
 		$.each(selectedMenu, function(index, dish) {
 			$dishPrice = 0;
 
+
+
 			$.each(dish.ingredients, function(i, o) {
 				$dishPrice += o.price;
 			});
+
 			$totalPrice += $dishPrice;
-			$("#selected-menu").append(
-				"<div class=\"col-sm-3\" id=\"selectedDish" + dish.id + "\">" +
-					"<div class=\"dish-header\">" +  
-						"<img src=\"images/" + dish.image + "\">" +
-						"<p>" + dish.name + "</p>" +
-					"</div>" +
-					"<p>" + $dishPrice + "</p>" +
-				"</div>"
-			);
+
+			//add vertical line to separate last course with total-price.
+			if(index === ($lengthOfArray - 1)) {
+				$("#selected-menu").append(
+					"<div class=\"col-sm-3 dish-list last-dish\" id=\"selected-dish-" + dish.id + "\">" +
+						"<div class=\"dish-header\">" +  
+							"<img src=\"images/" + dish.image + "\">" +
+							"<p>" + dish.name + "</p>" +
+						"</div>" +
+						"<p class=\"price-text\">" + $dishPrice + " SEK </p>" +
+					"</div>"
+				);
+			} else {
+
+				$("#selected-menu").append(
+					"<div class=\"col-sm-3 dish-list \" id=\"selected-dish-" + dish.id + "\">" +
+						"<div class=\"dish-header\">" +  
+							"<img src=\"images/" + dish.image + "\">" +
+							"<p>" + dish.name + "</p>" +
+						"</div>" +
+						"<p class=\"price-text\">" + $dishPrice + " SEK </p>" +
+					"</div>"
+				);
+			}
 		});
 
 		$("#selected-menu").append(
-			"<div class=\"col-sm-3\">" +
-				"<p>Total<p>" +
-				"<p>" + $totalPrice + "<p>" +
+			"<div id=\"total-price\">" +
+				"<p>Total:<p>" +
+				"<p class=\"price-text\">" + $totalPrice + " SEK<p>" +
 			"</div>"
 		);
 
