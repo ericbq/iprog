2f735d6ae6f57bdc16269821d1246d3491f1e3b8
ericbq <ericbl@kth.se>
2016-02-09 15:59:57 +0100

    dinner preparation html, css and js

---
+++ lab2/js/view/preparationView.js
@@ -0,0 +1,29 @@
+//DishView Object constructor
+var PreparationView = function (container, model, selectedMenu) {
+
+	$(document).ready(function() {
+		$("#overview-headline span").html( model.getNumberOfGuests() + " people");
+
+		$lengthOfArray = selectedMenu.length;
+		//model.getFullMenu();
+		$.each(selectedMenu, function(index, dish) {
+			$("#preparation-content table").append(
+				"<tr id=\"tablerow-dish-" + dish.id + "\">" +
+					"<td class=\"td-image\">" +  
+						"<img src=\"images/" + dish.image + "\">" +
+					"</td>" +
+					"<td class=\"td-image\">" +  
+						"<h2>" + dish.name + "</h2>" +
+						"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus turpis tristique arcu tempus commodo. Curabitur vel ex lorem. Curabitur porta tellus nunc. Aliquam aliquam elementum dui vitae tincidunt. Donec egestas in lorem ac finibus. Aenean molestie, nibh eget luctus gravida, leo quam fringilla sem, a sollicitudin nibh velit at enim. Nunc convallis tortor ac odio consequat vestibulum. Nulla facilisi.</p>" + 
+					"</td>" +
+					"<td>" +
+						"<h3> Preparation </h3>" + 
+						"<p>" + dish.description + "</p>" +
+					"</td>" +
+				"</tr>"
+			);
+		});
+
+	});
+}
+ 
